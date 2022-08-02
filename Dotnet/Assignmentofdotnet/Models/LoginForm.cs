d6bac71917faabadd47c87600791ec78efe73714
DAXshivam <109572368+DAXshivam@users.noreply.github.com>
2022-08-02 11:55:09 +0530

    Add files via upload
    Initial Push

---
+++ Dotnet/Assignmentofdotnet/Models/LoginForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Assignmentofdotnet.Models
+{
+    public class LoginForm
+    {
+        [Key]
+        public int Id { get; set; }
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ Dotnet/Assignmentofdotnet/Models/LoginFormContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignmentofdotnet.Models
+{
+    public class LoginFormContext : DbContext
+    {
+        public LoginFormContext(DbContextOptions<LoginFormContext> options): base(options)
+        {
+
+        }
+        public DbSet<LoginForm> LoginForm { get; set; }
+    }
+}
